46b89a403e9fba1feb6bb369bd1f6f02523f58f2
hhiroki <h-hiroki@users.noreply.github.com>
2018-10-28 14:39:50 +0900

    ルートとコントローラ作ってテストする

---
+++ task-manager/tests/Feature/TaskControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TaskControllerTest extends TestCase
+{
+    /**
+     * Get All Tasks Path Test
+     *
+     * @return void
+     */
+    public function testGetAllTasksPath()
+    {
+        $response = $this->get('/tasks');
+        $response->assertStatus(200);
+
+    }
+}
